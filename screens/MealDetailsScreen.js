81324790e4e37d6a51d86d7a58f57b0812949f92
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-07-25 18:27:45 -0700

    added screens

---
+++ screens/MealDetailsScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MealDetailsScreen = props => {
+    return(
+        <View style={styles.screen>
+          <Text> The Meal Detais Screen</Text>
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+
+});
+
+export default MealDetailsScreen;
