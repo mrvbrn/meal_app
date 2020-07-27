c65254a0c357f2c385ce8fd5cc0d6bd37974c42b
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-07-27 14:09:57 -0700

    navigate screen

--- screens/CategorysScreen.js
+++ screens/CategorysScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CategorysScreen = props => {
     return(
         <View style={styles.screen}>
           <Text> The Categorys Screen</Text>
+          <Button title="go meals app!" onPress={()=> props.navigation.navigate('CategoryMeals')}/>
         </View>
     );
 
