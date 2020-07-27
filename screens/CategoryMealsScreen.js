0189304107fc2535829c881d8799d58696ff21d2
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-07-27 14:36:56 -0700

    return main page

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -6,6 +6,7 @@ const CategoryMealsScreen = props => {
         <View style={styles.screen}>
           <Text> The Category Meals Screen</Text>
           <Button title="Go to Details" onPress={()=>props.navigation.navigate('MealDetail')}/>
+          <Button title="Go Back" onPress={()=>props.navigation.goBack()}/>
         </View>
     );
 
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const MealDetailsScreen = props => {
     return(
         <View style={styles.screen}>
           <Text> The Meal Detais Screen</Text>
+          <Button title="Go Back to Category" onPress={()=>props.navigation.popToTop()}/>
         </View>
     );
 
