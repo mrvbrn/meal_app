986060745444a75ba721f97ca7d3907b900cb20c
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-07-29 00:16:15 -0700

    default navigation options

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,9 +1,11 @@
+import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from 'react-navigation';
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import CategorysScreen from "../screens/CategorysScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
+import Colors from "../constants/Colors";
 
 
 const MealsNavigator = createStackNavigator({
@@ -12,7 +14,16 @@ const MealsNavigator = createStackNavigator({
         screen:CategoryMealsScreen
     },
     MealDetail:MealDetailsScreen
-});
+    },
+    {
+    defaultNavigationOptions:{
+        headerStyle:{
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+        }
+    }
+);
 
 
 export default createAppContainer(MealsNavigator);
--- screens/CategorysScreen.js
+++ screens/CategorysScreen.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Platform } from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity} from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
-import Colors from "../constants/Colors";
-
 
 const CategoriesScreen = props => {
   const renderGridItem = itemData => {
@@ -34,10 +32,6 @@ const CategoriesScreen = props => {
 
 CategoriesScreen.navigationOptions = {
     headerTitle : 'Meal Categories',
-    headerStyle:{
-        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-    },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 };
 
 const styles = StyleSheet.create({
