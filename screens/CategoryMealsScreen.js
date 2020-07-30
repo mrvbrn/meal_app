9e1dea6db2dbcab256ea13b84e3b2f410679e8d1
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-07-30 11:27:37 -0700

    loading meals for categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 const CategoryMealsScreen = props => {
+    const renderMealItem = itemData =>{
+        return(<View><Text>{itemData.item.title}</Text></View>)
+    }
+    const catID = props.navigation.getParam('categoryId');
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catID) >= 0)
     return(
         <View style={styles.screen}>
-          <Text> The Category Meals Screen</Text>
-          <Button title="Go to Details" onPress={()=>props.navigation.navigate('MealDetail')}/>
-          <Button title="Go Back" onPress={()=>props.navigation.goBack()}/>
+          <FlatList
+            data={displayedMeals}
+            keyExtractor={(item,index) => item.id}
+            renderItem={renderMealItem}
+          />  
         </View>
     );
 };
 
 CategoryMealsScreen.navigationOptions = navigationData => {
     const catID = navigationData.navigation.getParam('categoryId');
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catID);
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catID)
     return{
       headerTitle : selectedCategory.title,
     }
