7ff2931a6a3f7724283e389efdc57459518ef319
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-08-05 22:53:10 -0700

    installed redux and set up store

--- App.js
+++ App.js
@@ -3,7 +3,17 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from "expo";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
 import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/Meals"
+
+const rootReducer = combineReducers({
+  meals:mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -24,7 +34,7 @@ export default function App() {
     );
   }
   return (
-    <MealsNavigator/>
+    <Provider store={store}><MealsNavigator/></Provider>
   );
 }
 
